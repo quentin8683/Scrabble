3cbec86ecc22d9312c1a38d81be880f6d9ff3979
quentin8683 <quentin8683@gmail.com>
2026-09-02 15:05:55 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,67 @@
+import requests
+import json
+
+class ScrabbleClient:
+    def __init__(self, server_url):
+        self.server_url = server_url
+        self.player_index = None
+    
+    def join(self, name):
+        response = requests.post(
+            f"{self.server_url}/join",
+            json={"name": name}
+        )
+        data = response.json()
+        self.player_index = data.get('player_index')
+        return data
+    
+    def get_state(self):
+        response = requests.get(
+            f"{self.server_url}/state",
+            params={"player_index": self.player_index}
+        )
+        return response.json()
+    
+    def place(self, row, col, rack_index, joker_letter=None):
+        response = requests.post(
+            f"{self.server_url}/place",
+            json={
+                "player_index": self.player_index,
+                "row": row,
+                "col": col,
+                "rack_index": rack_index,
+                "joker_letter": joker_letter
+            }
+        )
+        return response.json()
+    
+    def play(self):
+        response = requests.post(
+            f"{self.server_url}/play",
+            json={"player_index": self.player_index}
+        )
+        return response.json()
+    
+    def pass_turn(self):
+        response = requests.post(
+            f"{self.server_url}/pass",
+            json={"player_index": self.player_index}
+        )
+        return response.json()
+    
+    def exchange(self, indices):
+        response = requests.post(
+            f"{self.server_url}/exchange",
+            json={
+                "player_index": self.player_index,
+                "indices": indices
+            }
+        )
+        return response.json()
+    
+    def cancel(self):
+        response = requests.post(
+            f"{self.server_url}/cancel",
+            json={"player_index": self.player_index}
+        )
+        return response.json()
